f310e364bccdc17b919c5a3d2489e9ef3f6f6882
Kavi Jivan <kavi@jivan.cc>
2021-05-08 01:01:17 -0400

    Removes slower zsh plugins git and kubectl for now

--- .zsh_plugins.sh
+++ .zsh_plugins.sh
@@ -4,9 +4,5 @@ source /Users/kavijivan/Library/Caches/antibody/https-COLON--SLASH--SLASH-github
 fpath+=( /Users/kavijivan/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
 source /Users/kavijivan/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
 fpath+=( /Users/kavijivan/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
-source /Users/kavijivan/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git/git.plugin.zsh
-fpath+=( /Users/kavijivan/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git )
-source /Users/kavijivan/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/kubectl/kubectl.plugin.zsh
-fpath+=( /Users/kavijivan/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/kubectl )
 source /Users/kavijivan/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips/alias-tips.plugin.zsh
 fpath+=( /Users/kavijivan/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips )
