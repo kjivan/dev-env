5e394d826a12d7492f6a53cd82a7c2080e45f5ca
Kavi Jivan <kjivan@gmail.com>
2021-12-22 09:15:22 -0800

    Adds docker aliases

--- .config/fish/docker.fish
+++ .config/fish/docker.fish
@@ -1,4 +1,12 @@
 function edit-docker -w "vi $HOME/.config/fish/docker.fish"; vi $HOME/.config/fish/docker.fish $argv; end
 
 function d -w "docker"; docker $argv; end
-function docker-stop-all -w "docker stop (docker ps -q)"; docker stop (docker ps -q) $argv; end
+
+function db -w "docker build ."; docker build $argv .; end
+
+function dr -w "docker run"; docker build run $argv; end
+
+function dbr -w "docker run -it (docker build -q .)"; docker run $argv -it (docker build -q .); end
+function dbrc -w "docker run --rm -it (docker build -q .)"; docker run $argv --rm -it (docker build -q .); end
+
+function docker-stop-all -w "docker stop (docker ps -q)"; docker stop (docker ps -q); end
