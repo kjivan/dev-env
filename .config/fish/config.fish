f32c5c3d54915b239b1721edc6e9faf5afb52989
Kavi Jivan <kjivan@gmail.com>
2022-01-04 09:21:11 -0800

    Comment out conda for now

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -133,5 +133,5 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /Users/kavijivan/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+#eval /Users/kavijivan/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
