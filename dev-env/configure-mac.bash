50d2c1ec6c6e0bdc9bcfe39b59d7b51637916b43
Kavi Jivan <kavi@jivan.cc>
2022-04-18 12:19:39 -0700

    Fixes change monitor shortcut

--- dev-env/configure-mac.bash
+++ dev-env/configure-mac.bash
@@ -15,6 +15,6 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 15
 defaults write -globalDomain NSUserKeyEquivalents -dict-add "Minimize" "@^\\U2193";
 defaults write -globalDomain NSUserKeyEquivalents -dict-add "Move Window to Left Side of Screen" "@^\\U2190";
 defaults write -globalDomain NSUserKeyEquivalents -dict-add "Move Window to Right Side of Screen" "@^\\U2192";
-defaults write -globalDomain NSUserKeyEquivalents -dict-add "Move to Built-in Retina Display" "@~\\U2190";
-defaults write -globalDomain NSUserKeyEquivalents -dict-add "Move to HS156KC" "@~\\U2192";
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "Move to Built-in Retina Display" "@$\\U2190";
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "Move to HS156KC" "@$\\U2192";
 defaults write -globalDomain NSUserKeyEquivalents -dict-add "Zoom" "@^\\U2191";
