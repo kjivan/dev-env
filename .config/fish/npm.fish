dd1e7e45e8fcc9b630d265d47f28d23e2764f7f1
Kavi Jivan <kavi@jivan.cc>
2022-05-28 15:23:33 -0700

    Adds more npm aliases

--- .config/fish/npm.fish
+++ .config/fish/npm.fish
@@ -1,6 +1,10 @@
 function edit-npm -w "vi $HOME/.config/fish/npm.fish"; vi $HOME/.config/fish/npm.fish $argv; end
 
 function ni -w "npm install"; npm install $argv; end
+function nod -w "npm outdated"; npm outdated $argv; end
+function nu -w "npm update"; npm update $argv; end
+function ncu -w "npx npm-check -u"; npx npm-check -u; end
+function npm-update-all -w "npx npm-check-updates -u"; npx npm-check-updates -u; end
 
 function nrs -w "npm run start"; npm run start $argv; end
 function nrb -w "npm run build"; npm run build $argv; end
@@ -8,4 +12,5 @@ function nrt -w "npm run test"; npm run test $argv; end
 function nrth -w "npm run test-headless"; npm run test-headless $argv; end
 function nrl -w "npm run lint"; npm run lint $argv; end
 function nre -w "npm run e2e"; npm run e2e $argv; end
+
 function acid-test-gyp -w "curl -sL https://github.com/nodejs/node-gyp/raw/master/macOS_Catalina_acid_test.sh | bash"; curl -sL https://github.com/nodejs/node-gyp/raw/master/macOS_Catalina_acid_test.sh | bash $argv; end
