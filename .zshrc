966e5f750496a8ca7bedf0050d51d82bfd00d32c
Kavi Jivan <kavi@jivan.cc>
2022-07-16 11:59:01 -0700

    Adds nvm, vscode-prettier, updated monitor config

--- .zshrc
+++ .zshrc
@@ -6,3 +6,7 @@ fi
 if command -v fish &> /dev/null; then
 	fish
 fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- dev-env/configure-mac.bash
+++ dev-env/configure-mac.bash
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-defaults write NSGlobalDomain KeyRepeat -int 2
-defaults write NSGlobalDomain InitialKeyRepeat -int 15
-
 defaults write -globalDomain NSUserKeyEquivalents -dict-add "Minimize" "@^\\U2193";
 defaults write -globalDomain NSUserKeyEquivalents -dict-add "Move Window to Left Side of Screen" "@^\\U2190";
 defaults write -globalDomain NSUserKeyEquivalents -dict-add "Move Window to Right Side of Screen" "@^\\U2192";
 defaults write -globalDomain NSUserKeyEquivalents -dict-add "Move to Built-in Retina Display" "@$\\U2190";
-defaults write -globalDomain NSUserKeyEquivalents -dict-add "Move to HS156KC" "@$\\U2192";
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "Move to LG Ultra HD" "@$\\U2192";
 defaults write -globalDomain NSUserKeyEquivalents -dict-add "Zoom" "@^\\U2191";
