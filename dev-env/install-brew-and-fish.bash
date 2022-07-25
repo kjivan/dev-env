bc4a3266747b6303268ca326702f8d5354874fdf
Kavi Jivan <kavi@jivan.cc>
2022-07-25 16:18:53 -0700

    Updates to env setup

--- dev-env/install-brew-and-fish.bash
+++ dev-env/install-brew-and-fish.bash
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install git
-git -C "$(brew --repo homebrew/core)" fetch --unshallow
 
 brew install fish starship
-sudo bash -c 'echo /usr/local/bin/fish >> /etc/shells'
-chsh -s /usr/local/bin/fish
+sudo bash -c 'echo /opt/homebrew/bin/fish >> /etc/shells'
+chsh -s /opt/homebrew/bin/fish
